6f4d1651890a8cef317b90976a5aa2fba3bbfb23
dcloutier <david.cloutier@oracle.com>
2018-10-05 22:57:50 -0400

    substracting 2 numbers, multiplying 2 numbers

--- issues_found.md
+++ issues_found.md
@@ -1,12 +1,20 @@
 #Functional issues found
+* Executing - - Operation yeilds an "ERR" result. 
+  (Substracting 2 negative numbers) You will see my test
+  fail because of that
 
 
+
+
+# Features Missing
 * There is no "clear" function available
     * For each of my tests, I need to either reload a new browser instance OR,
      make sure the equation field is empty. 
      
-     
+# Security     
 * The website where the calculator isn't using a valid SSL certificate
     - don't know if its intended, but seeing that browsers are moving
     towards trying to limit access to non HTTPS web sites, this might
     need to be looked at.  
+    
+    
--- specs/functional.rb
+++ specs/functional.rb
@@ -11,53 +11,112 @@ describe 'Integration Tests' do
   end
 
   after :each do
-    #empty_equation_field(@browser.div(id: 'output'), flash_and_ensure_click(@browser.button(value: 'DEL')))
+    #empty_equation_field(@browser.text_field(id: 'expression'), flash_and_ensure_click(@browser.button(value: 'DEL')))
 
     @browser.close
     @browser = nil
     # why closing the browser each time?
     # The output field isn't cleared by repeatedly pressing the DEL button via the automation
-    # It does work manually, but Watir see the field empty after the first 'DEL press'
+    # It does work manually, but Watir see the 'expression' field empty after the first 'DEL press'
     # See for yourself, uncomment, the first line and comment the close and nil statements
   end
 
   describe 'Functional Testing' do
-    describe 'validating the addition of 2 positive numbers' do
-      it 'Adding 2 + 2 should yield 4' do
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(value: 'EQ'))
-        expect(@browser.div(id: 'output').text.include? '4').to be true
+=begin
+    describe 'addition operations' do
+      describe 'validating the addition of 2 positive numbers' do
+        it 'Adding (2 + 2) should yield 4' do
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('4')
+        end
+      end
+      describe 'validating the addition of 2 negative numbers' do
+        it 'Adding (-2 + -2 )should yield -4' do
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('-4')
+        end
+      end
+      describe 'validating the addition of 1 postive number and negative number' do
+        it 'Adding (2 + -2) should yield 0' do
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('0')
+        end
+        it 'Adding (-2 + 2) should yield 0' do
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('0')
+        end
       end
     end
-    describe 'validating the addition of 2 negative numbers' do
-      it 'Adding -2 + -2 should yield -4' do
-        flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(value: 'EQ'))
-        expect(@browser.div(id: 'output').text.include? '-4').to be true
+    describe 'substration operations' do
+      describe 'validating the substraction of 2 positive numbers' do
+        it 'substracting (2 - 2) should yield 0' do
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('0')
+        end
+      end
+      describe 'validating the substration of 2 negative numbers' do
+        # this test WILL fail, as listed in the funtional issue list
+        it 'substracting (-2 - -2) should yield 0' do
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('0')
+        end
+      end
+      describe 'validating the substration of 1 negative and 1 positive number' do
+        it 'substracting (-2 - 2) should yield -4' do
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '5'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('-7')
+        end
       end
     end
-    describe 'validating the addition of 1 postive number and negative number' do
-      it 'Adding 2 + -2 should yield 0' do
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(value: 'EQ'))
-        expect(@browser.div(id: 'output').text.include? '0').to be true
+=end
+    describe 'multiplications operations' do
+      describe 'validating the multiplication of 2 positive numbers' do
+        it 'substracting (2 x 2) should yield 2' do
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('4')
+        end
       end
-      it 'Adding -2 + 2 should yield 0' do
-        flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
-        flash_and_ensure_click(@browser.button(value: '2'))
-        flash_and_ensure_click(@browser.button(value: 'EQ'))
-        expect(@browser.div(id: 'output').text.include? '0').to be true
+      describe 'validating the multiplication of 2 negative numbers' do
+        it 'substracting (-2 x -2) should yield 2' do
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('4')
+        end
       end
     end
   end
