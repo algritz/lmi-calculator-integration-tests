8667fb61ce01f690e580c397c767573d2abb316d
dcloutier <david.cloutier@oracle.com>
2018-10-05 23:50:13 -0400

    added multiply div an egge cases

--- specs/functional.rb
+++ specs/functional.rb
@@ -75,7 +75,7 @@ describe 'Integration Tests' do
       end
       describe 'validating the substration of 2 negative numbers' do
         # this test WILL fail, as listed in the funtional issue list
-        it 'substracting (-2 - -2) should yield 0' do
+        it 'substracting (-2 - -2) should yield 0 (This test WILL FAIL)' do
           flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
           flash_and_ensure_click(@browser.button(value: '2'))
           flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
@@ -96,10 +96,9 @@ describe 'Integration Tests' do
         end
       end
     end
-=end
     describe 'multiplications operations' do
       describe 'validating the multiplication of 2 positive numbers' do
-        it 'substracting (2 x 2) should yield 2' do
+        it 'multiplicating (2 x 2) should yield 2' do
           flash_and_ensure_click(@browser.button(value: '2'))
           flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
           flash_and_ensure_click(@browser.button(value: '2'))
@@ -108,7 +107,7 @@ describe 'Integration Tests' do
         end
       end
       describe 'validating the multiplication of 2 negative numbers' do
-        it 'substracting (-2 x -2) should yield 2' do
+        it 'multiplicating (-2 x -2) should yield 2' do
           flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
           flash_and_ensure_click(@browser.button(value: '2'))
           flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
@@ -118,6 +117,126 @@ describe 'Integration Tests' do
           expect(@browser.div(id: 'output').text).to eq('4')
         end
       end
+      describe 'validating the multiplication of 1 possitive anb 1 negative number' do
+        it 'multiplicating (1 x -2) should yield -2' do
+          flash_and_ensure_click(@browser.button(value: '1'))
+          flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('-2')
+        end
+      end
+    end
+
+    describe 'divisions operations' do
+      describe 'validating the division of 2 positive numbers' do
+        it 'dividing (2 / 2) should yield 1' do
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('1')
+        end
+      end
+      describe 'validating the division of 2 negative numbers' do
+        it 'dividing (-2 / -2) should yield 1' do
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('1')
+        end
+      end
+      describe 'validating the division of 1 positive and 1 negative number' do
+        it 'dividing (2 / -1) should yield -1' do
+          flash_and_ensure_click(@browser.button(value: '2'))
+          flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+          flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+          flash_and_ensure_click(@browser.button(value: '1'))
+          flash_and_ensure_click(@browser.button(value: 'EQ'))
+          expect(@browser.div(id: 'output').text).to eq('-2')
+        end
+      end
+=end
+  end
+
+  describe 'Edge cases' do
+    describe 'validating the division by 0 of any numbers' do
+      it 'dividing any number ny 0 should yield an ERR result' do
+        # ok, some fluff here. I could have used any static number here, but hey, life's random at times. :-D
+        flash_and_ensure_click(@browser.button(value: rand(1..9).to_i.to_s))
+        flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '0'))
+        flash_and_ensure_click(@browser.button(value: 'EQ'))
+        expect(@browser.div(id: 'output').text).to eq('Infinity')
+      end
+    end
+    describe 'validating the division by a positive number of a negative' do
+      it 'dividing (2 by -2) should yield -1 result' do
+        # ok, some fluff here. I could have used any static number here, but hey, life's random at times. :-D
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+        flash_and_ensure_click(@browser.button(text: 'MINUS', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(value: 'EQ'))
+        expect(@browser.div(id: 'output').text).to eq('-1')
+      end
+    end
+    describe 'validating the division zero by number of any numbers' do
+      it 'dividing any number ny 0 should yield 0 as result' do
+        # more random fluff
+        flash_and_ensure_click(@browser.button(value: '0'))
+        flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: rand(1..9).to_i.to_s))
+        flash_and_ensure_click(@browser.button(value: 'EQ'))
+        expect(@browser.div(id: 'output').text).to eq('0')
+      end
+    end
+    describe 'Respecting the order of operations' do
+      it 'Should respect th order or operation implicating multiplications' do
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(value: 'EQ'))
+        expect(@browser.div(id: 'output').text).to eq('8')
+      end
+
+      it 'Should respect the order or operation implicating division' do
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(value: 'EQ'))
+        expect(@browser.div(id: 'output').text).to eq('5')
+      end
+
+
+      it 'l/f 1' do
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'PLUS', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(text: 'MULT', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '8'))
+        flash_and_ensure_click(@browser.button(text: 'DIV', class: 'operation'))
+        flash_and_ensure_click(@browser.button(value: '2'))
+        flash_and_ensure_click(@browser.button(value: 'EQ'))
+        expect(@browser.div(id: 'output').text).to eq('14')
+      end
+
     end
   end
 end
